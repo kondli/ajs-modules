fbb3d67dab25db39d129fc9c8797a8b93f249f64
Liliia Kondratskaia <sirc31647281@yandex.ru>
2022-06-30 11:51:35 +0300

    1,2 tasks done

--- src/js/game.js
+++ src/js/game.js
@@ -1,16 +1,16 @@
 import Character from './domain';
 
 export default class Game {
-    start() {
-      console.log('game started');
-    }
+  start() {
+    console.log('game started');
   }
-  
+}
+
 export class GameSavingData {
-  }
-  
-export  function readGameSaving() {
-  }
-  
-export  function writeGameSaving() {
-  }
+}
+
+export function readGameSaving() {
+}
+
+export function writeGameSaving() {
+}
